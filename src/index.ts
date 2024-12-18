d099741720755ce53c5f353941f4aa6f2fb02585
Dmitry Vasiliev <dmitry.vasiliev@broit.io>
2024-12-19 01:24:26 +0300

    feat: add bunny pile

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,10 @@ import { Stats } from 'pixi-stats';
 import {
   Application,
   Assets,
-  Geometry,
-  Mesh,
-  Shader,
-  Texture,
+  Sprite
 } from "pixi.js";
 
-import vertexShader from "./base.vert?raw";
 
-import fragmentShader from "./base.frag?raw";
 
 async function main() {
   // Create PixiJS application
@@ -26,49 +21,23 @@ async function main() {
   await Assets.load("peach.webp");
 
   const stats = new Stats(app.renderer);
-
-  const shader = Shader.from(vertexShader, fragmentShader, {
-    uSampler2: Assets.cache.get("bunny.webp") as Texture,
-  });
-
-  const geometry = new Geometry()
-    .addAttribute(
-      "aVertexPosition", // the attribute name
-      [
-        -100,
-        -100, // bottom-left
-        100,
-        -100, // bottom-right
-        100,
-        100, // top-right
-        -100,
-        100, // top-left
-      ],
-      2 // size of each vertex (x, y)
-    )
-    .addAttribute(
-      "aUvs", // the attribute name
-      [
-        0,
-        0, // bottom-left
-        1,
-        0, // bottom-right
-        1,
-        1, // top-right
-        0,
-        1, // top-left
-      ],
-      2 // size of each UV (u, v)
-    )
-    .addIndex([0, 1, 2, 0, 2, 3]); // indices that form two triangles for a quad
+app.stage.scale.set(0.75);
+app.stage.position.set(app.renderer.width/2 , app.renderer.height/2 )
 
     for (let i = 0; i < 100;i++){
-      const example = new Mesh(geometry, shader);
+      const example = new Sprite(Assets.cache.get("bunny.webp"));
 
-      example.position.set(app.renderer.width/2 - app.renderer.width/3 + app.renderer.width*2/3 *Math.random() , app.renderer.height/2- app.renderer.height/3 + app.renderer.height*2/3 *Math.random());
+      example.position.set(-app.renderer.width/2 + app.renderer.width/2 *Math.random() , -app.renderer.height/2 + app.renderer.height/2 *Math.random());
 
       app.stage.addChild(example);
     }
+
+    app.ticker.add((dt)=>{
+      app.stage.children.forEach((a)=>{
+        a.x += dt * (-Math.PI + 2 * Math.PI * Math.random());
+        a.y += dt * (-Math.PI + 2 * Math.PI * Math.random());
+      })
+    })
 
 }
 
